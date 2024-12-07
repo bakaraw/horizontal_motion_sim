aa62b10bb690cd55f56ee56be7eb1f8020cc33ac
linux bakaraw <bacalso.ton2x@gmail.com>
2024-12-08 01:13:04 +0800

    add: add text to show a, v, and x

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import pygame
 from parallax import Parallax
-from typing import List
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -9,9 +8,8 @@ FPS = 60
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    bg_layers = []
 
-    parallax = Parallax(SCREEN_WIDTH, SCREEN_HEIGHT, [
+    parallax = Parallax(SCREEN_WIDTH, [
         "assets/far_clouds.png", 
         "assets/near_clouds.png", 
         "assets/far_mountains.png",
@@ -21,10 +19,13 @@ def main():
 
     bg_position_x = 0
     velocity = 0
-    accelaration = 0
-    max_velocity = 200
+    accelaration = 10
+    max_velocity = 80
+    total_distance = 0
     running = True
     clock = pygame.time.Clock()
+
+    font = pygame.font.Font(None, 24)
 
     # Main game loop
     while running:
@@ -33,11 +34,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            accelaration += 10
-        if key[pygame.K_DOWN]:
-            accelaration -= 10
+        # key = pygame.key.get_pressed()
+        # if key[pygame.K_UP]:
+        #     accelaration += 10
+        # if key[pygame.K_DOWN]:
+        #     accelaration -= 10
 
         # change in time
         delta_time = clock.tick(FPS) / 1000
@@ -49,12 +50,15 @@ def main():
             velocity = 0
 
         # formula: (x = x0 + v * t)
+        print(delta_time)
         bg_position_x += velocity * delta_time
-        print(velocity)
+        total_distance += velocity * delta_time
 
         draw_sky(screen)
         parallax.draw(screen, bg_position_x)
-
+        draw_font(f"Acceleration: {accelaration:.2f} m^2/s^2", screen, font, 10, 10)
+        draw_font(f"Velocity: {velocity:.2f} m/s", screen, font, 10, 35)
+        draw_font(f"Distance Traveled: {total_distance:.2f} m", screen, font, 10, 60)
         pygame.draw.rect(screen, (44, 37, 70), (0, SCREEN_HEIGHT - 50, SCREEN_WIDTH, 50))
 
         van_sprite = pygame.image.load("assets/van_frames/frame000.png")
@@ -73,6 +77,10 @@ def draw_sky(screen):
     aspect_ratio = width / height
     sky = pygame.transform.scale(sky, (SCREEN_WIDTH, int(SCREEN_WIDTH / aspect_ratio)))
     screen.blit(sky, (0, 0))
+
+def draw_font(text, screen, font, x, y, color = (255, 255, 255)):
+    text = font.render(text, True, color)
+    screen.blit(text, (x, y))
 
 
 if __name__ == "__main__":
--- src/parallax.py
+++ src/parallax.py
@@ -4,7 +4,7 @@ from typing import List
 class Parallax:
     bg_layers = []
 
-    def __init__(self, screen_width, screen_height, image_paths: List[str]):
+    def __init__(self, screen_width, image_paths: List[str]):
         for i, path in enumerate(image_paths):
             length = len(image_paths)
             image = pygame.image.load(path)
@@ -17,6 +17,6 @@ class Parallax:
     def draw(self, screen, scroll):
         for layer, speed_multiplier in self.bg_layers:
             layer_width = layer.get_width()
-            x_offset = -(scroll * speed_multiplier) % layer_width
+            x_offset = -(scroll * 4 * speed_multiplier) % layer_width
             screen.blit(layer, (x_offset, 0))
             screen.blit(layer, (x_offset - layer_width, 0))
